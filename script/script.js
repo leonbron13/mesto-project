bec72d0d434cf3bc50754dd74a429c3c3ac597db
Bronnikov Leonid <123650306+leonbron13@users.noreply.github.com>
2023-04-26 19:16:25 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,108 @@
+const popup = document.querySelector('.popup'); 
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close-button');
+
+function popupOpened (){
+    popup.classList.add('popup_opened');
+};
+function popupClose (){
+    popup.classList.remove('popup_opened');
+};
+
+editButton.addEventListener('click', popupOpened);
+closeButton.addEventListener('click', popupClose);
+////-----------------------------------------------------------------------EDITINFORM-----------------------------------------------------------------------------//
+const saveButton = document.querySelector('.adding_button');
+const cardsContainer = document.querySelector('.cards');
+
+
+function addCard(titleValue, imageValue){
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__img').src = imageValue;
+    cardElement.querySelector('.card__title').textContent = titleValue;
+
+    cardsContainer.append(cardElement);
+}
+
+saveButton.addEventListener('click', function (e) {
+    e.preventDefault()
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.cloneNode(true); 
+    console.log(cardElement)
+    const cardTitle = document.querySelector('.profile__popup-title');
+    const cardImg = document.querySelector('.profile__popup-image');
+    let newCardTitle  = cardTitle.value;
+    let newCardImg = cardImg.value;
+    cardElement.querySelector('.card__title').textContent = newCardTitle;
+    cardElement.querySelector('.card__img').src = newCardImg;
+    profile.classList.remove('profile__form_opened');
+
+
+
+
+    cardsContainer.insertBefore(cardElement, cardsContainer.firstChild);
+  });
+////-----------------------------------------------------------------------LIKES-----------------------------------------------------------------------------//
+
+const likes = document.querySelectorAll('.card__like-button');
+for (let i = 0; i < likes.length; i++){
+    likes[i].addEventListener('click', function(){ 
+    likes[i].classList.toggle('card__like-button_checked')
+    console.log(123)
+})
+}
+
+////-----------------------------------------------------------------------EDITFORM-----------------------------------------------------------------------------//
+const profile = document.querySelector('.profile__form')
+const addButton = document.querySelector('.profile__add-button'); 
+const closeForm = document.querySelector('.profile__close-button');
+
+function addForm (){
+    profile.classList.add('profile__form_opened');
+};
+function formClose (){
+    profile.classList.remove('profile__form_opened');
+};
+addButton.addEventListener('click', addForm);
+closeForm.addEventListener('click', formClose);
+
+////-----------------------------------------------------------------------CARDIMAGE-----------------------------------------------------------------------------//
+const imageCollection = document.querySelectorAll('.card__img');
+const textCollection = document.querySelectorAll('.card__title');
+const imagePopup = document.querySelector('.popup__open-image');
+const imgsrc = imagePopup.querySelector('.popup__image');
+const closeCard = document.querySelector('.popup__close-button_image');
+const imgText = document.querySelector('.popup__subtitle');
+
+for (let i = 0; i < imageCollection.length; i++ ){
+    imageCollection[i].addEventListener('click', function(){
+        imagePopup.classList.add('popup_opened');
+        imgsrc.src = imageCollection[i].src;
+        imgText.textContent = textCollection[i].textContent;
+
+    });
+
+}
+function cardClose (){
+    imagePopup.classList.remove('popup_opened')
+};
+closeCard.addEventListener('click', cardClose);
+////-----------------------------------------------------------------------CARDDELETE-----------------------------------------------------------------------------//
+const cardCollection = document.querySelectorAll('.card')
+const trashes = document.querySelectorAll('.card__trash')
+
+
+for (let i = 0; i < trashes.length; i++ ){
+    trashes[i].addEventListener('click', function(){
+        //cardsContainer.classList.remove(cardCollection[i]);
+        console.log(123)
+    });
+
+}
+
+
+
+
+
