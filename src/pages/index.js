7943091643aa1a80183cb6294f024efa0b2d7721
Bronnikov Leonid <123650306+leonbron13@users.noreply.github.com>
2023-08-26 12:55:51 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,63 +1,6 @@
 import '../pages/index.css';
 
 
-const interBlack = new URL('../vendor/fonts/Inter-Black.woff', import.meta.url);
-const interBlack2 = new URL('../vendor/fonts/Inter-Black.woff2', import.meta.url);
-const interMedium = new URL('../vendor/fonts/Inter-Medium.woff', import.meta.url);
-const interMedium2 = new URL('../vendor/fonts/Inter-Medium.woff2', import.meta.url);
-const interRegular = new URL('../vendor/fonts/Inter-Regular.woff', import.meta.url);
-const interRegular2 = new URL('../vendor/fonts/Inter-Regular.woff2', import.meta.url);
-
-const add = new URL('../images/add.svg', import.meta.url);
-const arkhyz = new URL('../images/arkhyz.jpg', import.meta.url);
-const arkhyzSvg = new URL('../images/arkhyz.svg', import.meta.url)
-const avatar = new URL('../images/avatar.jpg', import.meta.url)
-const baikal = new URL('../images/baikal.jpg', import.meta.url)
-const chelyabinsk = new URL('../images/chelyabinsk-oblast.jpg', import.meta.url)
-const close = new URL('../images/close.svg', import.meta.url)
-const dombai = new URL('../images/dombai.jpg', import.meta.url)
-const edit = new URL('../images/edit.svg', import.meta.url)
-const elbrus = new URL('../images/elbrus.jpg', import.meta.url)
-const ivanovo = new URL('../images/ivanovo.jpg', import.meta.url)
-const kamchatka = new URL('../images/kamchatka.jpg', import.meta.url)
-const karachaevo = new URL('../images/karachaevo.jpg', import.meta.url)
-const kholmogorsky = new URL('../images/kholmogorsky-rayon.jpg', import.meta.url)
-const like_cheched = new URL('../images/like_cheched.svg', import.meta.url)
-const like = new URL('../images/like.svg', import.meta.url)
-const logo = new URL('../images/logo.svg', import.meta.url)
-const trash = new URL('../images/trash.svg', import.meta.url)
-const Vector1 = new URL('../images/Vector1.png', import.meta.url)
-
-const whoIsTheGoat = [
-  // меняем исходные пути на переменные
-  { name: 'add', image: add },
-  { name: 'arkhyz', image: arkhyz },
-  { name: 'arkhyzSvg', image: arkhyzSvg },
-  { name: 'avatar', image: avatar },
-  { name: 'baikal', image: baikal },
-  { name: 'chelyabinsk', image: chelyabinsk },
-  { name: 'close', image: close },
-  { name: 'dombai', image: dombai },
-  { name: 'edit', image: edit },
-  { name: 'elbrus', image: elbrus },
-  { name: 'ivanovo', image: ivanovo },
-  { name: 'kamchatka', image: kamchatka },
-  { name: 'karachaevo', image: karachaevo },
-  { name: 'kholmogorsky', image: kholmogorsky },
-  { name: 'like_cheched', image: like_cheched },
-  { name: 'like', image: like },
-  { name: 'logo', image: logo },
-  { name: 'trash', image: trash },
-  { name: 'Vector1', image: Vector1 },
-  { name: 'interBlack', link: interBlack },
-  { name: 'interBlack2', link: interBlack2 },
-  { name: 'interMedium', link: interMedium },
-  { name: 'interMedium2', link: interMedium2 },
-  { name: 'interRegular', link: interRegular },
-  { name: 'interRegular2', link: interRegular2 },
-]; 
-
-
 import { 
   deleteCard, 
   handleTrashClick, 
@@ -102,6 +45,36 @@ toggleButtonState
     overlay, 
     esc
   } from "../components/modal.js"
+const interBlack = new URL('../vendor/fonts/Inter-Black.woff', import.meta.url);
+const interBlack2 = new URL('../vendor/fonts/Inter-Black.woff2', import.meta.url);
+const interMedium = new URL('../vendor/fonts/Inter-Medium.woff', import.meta.url);
+const interMedium2 = new URL('../vendor/fonts/Inter-Medium.woff2', import.meta.url);
+const interRegular = new URL('../vendor/fonts/Inter-Regular.woff', import.meta.url);
+const interRegular2 = new URL('../vendor/fonts/Inter-Regular.woff2', import.meta.url);
+
+const add = new URL('../images/add.svg', import.meta.url);
+const arkhyz = new URL('../images/arkhyz.jpg', import.meta.url);
+const arkhyzSvg = new URL('../images/arkhyz.svg', import.meta.url)
+const avatar = new URL('../images/avatar.jpg', import.meta.url)
+const baikal = new URL('../images/baikal.jpg', import.meta.url)
+const chelyabinsk = new URL('../images/chelyabinsk-oblast.jpg', import.meta.url)
+const close = new URL('../images/close.svg', import.meta.url)
+const dombai = new URL('../images/dombai.jpg', import.meta.url)
+const edit = new URL('../images/edit.svg', import.meta.url)
+const elbrus = new URL('../images/elbrus.jpg', import.meta.url)
+const ivanovo = new URL('../images/ivanovo.jpg', import.meta.url)
+const kamchatka = new URL('../images/kamchatka.jpg', import.meta.url)
+const karachaevo = new URL('../images/karachaevo.jpg', import.meta.url)
+const kholmogorsky = new URL('../images/kholmogorsky-rayon.jpg', import.meta.url)
+const like_cheched = new URL('../images/like_cheched.svg', import.meta.url)
+const like = new URL('../images/like.svg', import.meta.url)
+const logo = new URL('../images/logo.svg', import.meta.url)
+const trash = new URL('../images/trash.svg', import.meta.url)
+const Vector1 = new URL('../images/Vector1.png', import.meta.url)
+
+
+
+
 
   renderInitialCards(initialCards);
   addButton.addEventListener('click', addForm); 
